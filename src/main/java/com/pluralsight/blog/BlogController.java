e1da84ced7adf11fbcf83d38edbbe97f96a35205
lakshmantgld <diwaakartg@gmail.com>
2020-03-28 13:52:46 +0900

    added list posts

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,16 +1,28 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
 
-    @RequestMapping("/")
-    public String listPosts(ModelMap obj) {
-        obj.put("title", "Blog Post 1");
-        return "home";
+    private PostRepository postRepository;
+
+    public BlogController (PostRepository obj) {
+        postRepository = obj;
     }
 
+    @RequestMapping("/")
+    public String listPosts(ModelMap obj) {
+
+        List<Post> listPostObj = postRepository.getAllPosts();
+
+        obj.put("posts", "listPostObj");
+        return "home";
+    }
 }
